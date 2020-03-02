30aa8b3471aa06be2e4a1ddb7ab53ff367ee5294
LEOPC\leo <leon.sandoval@alumnos.duoc.cl>
2020-03-02 20:00:26 -0300

    build

---
+++ build/precache-manifest.3121d76bcee35f3cc7d051e1ead73e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5266b21e9170de815c43011b4977b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ab1671d066eda3a3e0a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5ab1671d066eda3a3e0a",
+    "url": "/static/js/2.883cd5d9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.883cd5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ea781e33846fd191a9",
+    "url": "/static/js/main.558810ac.chunk.js"
+  },
+  {
+    "revision": "318384a4359c800c2137",
+    "url": "/static/js/runtime-main.72da31ae.js"
+  },
+  {
+    "revision": "28ba849af0237ccbd09326655ac7ad4a",
+    "url": "/static/media/ilustracion_inicio.28ba849a.png"
+  }
+]);
